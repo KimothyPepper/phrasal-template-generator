a9e52837676fc4226f20c6484613364ba7da1690
KimothyPepper <kim-pep@hotmail.com>
2016-02-26 10:43:38 -0500

    Fixed If statements

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ var verbed = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like?\na) Snacking\n b) Escaping\n c) Winning\n Enter the letter below:');
 
-if (story == a) {
-  document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + verbed + 'for hours.');
+if (story == 'a') {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
 }
 
-if (story == b) {
-  document.write('Ameilia' + verbed + 'through the' + adjective + colour + 'nebula escaping the space' + creature +'.');
+if (story == 'b') {
+  document.write('Ameilia ' + verbed + ' through the ' +  adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
 }
 
-if (story == c) {
-  document.write('Jackson chose his' + adjective + ',' + colour + creature + 'card and' + verbed + 'it to the table knowing he won.');
+if (story == 'c') {
+  document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
 }
